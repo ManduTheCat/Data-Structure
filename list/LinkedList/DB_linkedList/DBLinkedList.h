9c366cbaccc19c84f9566288ae1ef35a5e79ffc0
ManduTheCat <krocd@naver.com>
2021-07-19 19:15:42 +0900

    add DBlinked list

---
+++ list/LinkedList/DB_linkedList/DBLinkedList.h
@@ -0,0 +1,33 @@
+#ifndef __DB_LINKED_LIST__H_
+#define __DB_LINKED_LIST__H_
+
+#define TRUE	1;
+#define FALE	2;
+
+typedef int Data;
+
+typedef struct __node
+{
+	Data data;
+	struct __node *next;
+	struct __node *prev;
+
+} Node;
+
+typedef struct __DLinkedList
+{
+	Node *head;
+	Node *tail;
+	int numOfData;
+}DBLinkedList;
+
+typedef DBLinkedList List;
+
+void ListInit(List *plist);
+void ListInsert(List *plist, Data pdata);//데이터를 입력
+void ListFirst(List *plist, Data *pdata);//데이터를 다음에 조회 할거다 변수 유지된 상태에서 값만 바뀌는 구조지향
+void ListNext(List *plist, Data *pdata);
+int LPrevious(List *plist, Data *pdata); 
+int LCount(List*plist);
+
+#endif
