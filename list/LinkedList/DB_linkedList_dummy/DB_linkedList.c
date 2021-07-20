5dda806ea4dd9f3c490e66d968c186a3fce9c424
ManduTheCat <krocd@naver.com>
2021-07-20 23:46:56 +0900

    dbdummy remove fn

--- list/LinkedList/DB_linkedList_dummy/DB_linkedList.c
+++ list/LinkedList/DB_linkedList_dummy/DB_linkedList.c
@@ -7,12 +7,18 @@ void ListInit(list *plist)
 	plist->nomOfNdoe = 0;
 	plist->head = (Node*)malloc(sizeof(Node));
 	plist->tail = (Node*)malloc(sizeof(Node));
-	plist->head->Next = NULL;
-	plist->tail->prev = NULL;
+	plist->head->Next = plist->tail;
+	plist->head->prev = NULL;
+	plist->tail->prev = plist->head;
+	plist->tail->Next = NULL;
 }
 
-void ListInsert(list *plist, Data data)
+void ListInsert(list *plist, Data pdata)
 {
 	Node *newNode = (Node*)malloc(sizeof(Node));
-
+	newNode->data = pdata;
+	newNode->prev = plist->tail->prev;
+	plist->tail->prev->Next = newNode;
+	newNode->Next = plist->tail;
 }
+
